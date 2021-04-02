261f6a7e428c867993192beaa784d0b82dc5165d
Daejong Bang <bangdj@brandi.co.kr>
2021-04-02 11:20:34 +0900

    structure

--- src/components/ComponentHeader.vue
+++ src/components/ComponentHeader.vue
@@ -12,6 +12,6 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
+<style lang="scss" scoped>
+@import '~@/assets/scss/components/header';
 </style>
