40d61ec61d371419ea0b7e9f07304dc3d616cb55
kimjw5 <81278777+brandikimjw@users.noreply.github.com>
2021-04-05 14:33:16 +0900

    tag

--- src/components/PageMain.vue
+++ src/components/PageMain.vue
@@ -1,8 +1,518 @@
 <template>
-  <div>
-    main contents
-    <a href="#">link contents</a> <!--  -->
-    <img src="@/assets/images/logo.png" alt="vue" />
+  <div class="main-wrapper">
+    <!-- main-banner swiper -->
+    <div class="main-banner"></div>
+
+    <!-- TODAY 베스트 -->
+    <article class="main-products-nav">
+      <div class="title-wrap">
+        <h2 class="title">TODAY 베스트</h2>
+        <ul class="nav">
+          <li>전체</li>
+          <li>쇼핑몰마켓</li>
+          <li>브랜드</li>
+          <li>뷰티</li>
+        </ul>
+      </div>
+
+      <!-- list이미지 -->
+      <div class="main-products-list">
+        <ul class="main-list">
+          <!-- list1 -->
+          <li class="thumb">
+            <a href="#">
+              <figure class="figure">
+                  <img src="#" alt="미쏘 글로우 플리츠">
+              </figure>
+              <div class="bage">
+                  <span class="bage">1</span>
+              </div>
+              <div class="info">
+                  <p>미쏘</p>
+                  <strong>글로우 플리츠 스커트</strong>
+                  <span>17%</span>
+                  <em>25,560</em>
+              </div>
+            </a>
+          </li>
+          <!-- list2 -->
+          <li class="thumb">
+            <a href="#">
+              <figure class="figure">
+                  <img src="#" alt="알파카 부들 도톰니트">
+              </figure>
+              <div class="bage">
+                  <span class="bage">2</span>
+              </div>
+              <div class="info">
+                  <p>글로우프로젝트</p>
+                  <strong>알파카 부들 도톰니트</strong>
+                  <em>44,000</em>
+              </div>
+            </a>
+          </li>
+          <!-- lsit3 -->
+          <li class="thumb">
+            <a href="#">
+              <figure class="figure">
+                  <img src="#" alt="글로우 플리츠 스커트">
+              </figure>
+              <div class="bage">
+                  <span class="bage">3</span>
+              </div>
+              <div class="info">
+                  <p>프롬헤드투토</p>
+                  <strong>글로우 플리츠 스커트</strong>
+                  <span>11%</span>
+                  <em>32,650</em>
+              </div>
+            </a>
+          </li>
+          <!-- list4 -->
+          <li class="thumb">
+            <a href="#">
+              <figure class="figure">
+                  <img src="#" alt="헨드메이드 숏 코트">
+              </figure>
+              <div class="bage">
+                  <span class="bage">4</span>
+              </div>
+              <div class="info">
+                  <p>디어썸띵</p>
+                  <strong>핸드메이드 숏 코트 [3color]</strong>
+                  <span>10%</span>
+                  <em>118,000</em>
+              </div>
+            </a>
+          </li>
+          <!-- lsit5 -->
+          <li class="thumb">
+            <a href="#">
+              <figure class="figure">
+                  <img src="#" alt="[이너로 좋아요] 도톰 코튼…">
+              </figure>
+              <div class="bage">
+                  <span class="bage">5</span>
+              </div>
+              <div class="info">
+                  <p>모디데이</p>
+                  <strong>[이너로 좋아요] 도톰 코튼…</strong>
+                  <em>18,000</em>
+              </div>
+            </a>
+          </li>
+          <!-- list6 -->
+          <li class="thumb">
+            <a href="#">
+              <figure class="figure">
+                  <img src="#" alt="코튼 일자 롱 스커트">
+              </figure>
+              <div class="bage">
+                  <span class="bage">6</span>
+              </div>
+              <div class="info">
+                  <p>프롬비기닝</p>
+                  <strong>코튼 일자 롱 스커트</strong>
+                  <span>10%</span>
+                  <em>27,000</em>
+              </div>
+            </a>
+          </li>
+          <!-- list7 -->
+          <li class="thumb">
+            <a href="#">
+              <figure class="figure">
+                  <img src="#" alt="캔디블 도톰 루즈핏 가디건">
+              </figure>
+              <div class="bage">
+                  <span class="bage">7</span>
+              </div>
+              <div class="info">
+                  <p>모디데이</p>
+                  <strong>캔디블 도톰 루즈핏 가디건</strong>
+                  <em>39,900</em>
+              </div>
+            </a>
+          </li>
+          <!-- list8 -->
+          <li class="thumb">
+            <a href="#">
+              <figure class="figure">
+                  <img src="#" alt="도톰 부들 울 루즈핏 니트">
+              </figure>
+              <div class="bage">
+                  <span class="bage">8</span>
+              </div>
+              <div class="info">
+                  <p>프롬비기닝</p>
+                  <strong>도톰 부들 울 루즈핏 니트</strong>
+                  <span>10%</span>
+                  <em>27,000</em>
+              </div>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </article>
+
+    <div class="btn-common-main">
+      <a href="#"><span>베스트</span>더보기 ></a>
+    </div>
+
+    <!-- 신상 모아보기 -->
+    <article class="main-products new">
+      <div class="title-wrap">
+        <h2 class="title">신상 모아보기</h2>
+        <ul class="nav">
+          <li>쇼핑몰 마켓</li>
+          <li>브랜드</li>
+          <li>뷰티</li>
+        </ul>
+      </div>
+      <!-- list이미지 -->
+      <div class="main-products-list">
+        <ul class="main-list">
+          <!-- list1 -->
+          <li class="thumb">
+            <a href="#">
+              <figure class="figure">
+                  <img src="#" alt="미쏘 글로우 플리츠">
+              </figure>
+              <div class="info">
+                  <p>미쏘</p>
+                  <strong>글로우 플리츠 스커트</strong>
+                  <span>17%</span>
+                  <em>25,560</em>
+              </div>
+            </a>
+          </li>
+          <!-- list2 -->
+          <li class="thumb">
+            <a href="#">
+              <figure class="figure">
+                  <img src="#" alt="알파카 부들 도톰니트">
+              </figure>
+              <div class="info">
+                  <p>글로우프로젝트</p>
+                  <strong>알파카 부들 도톰니트</strong>
+                  <em>44,000</em>
+              </div>
+            </a>
+          </li>
+          <!-- lsit3 -->
+          <li class="thumb">
+            <a href="#">
+              <figure class="figure">
+                  <img src="#" alt="글로우 플리츠 스커트">
+              </figure>
+              <div class="info">
+                  <p>프롬헤드투토</p>
+                  <strong>글로우 플리츠 스커트</strong>
+                  <span>11%</span>
+                  <em>32,650</em>
+              </div>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </article>
+
+    <div class="btn-common-main">
+      <a href="#"><span>브랜드 신상</span>더보기 ></a>
+    </div>
+
+    <!-- 서브배너 -->
+    <aside class="coupon-banner"></aside>
+
+    <!-- 당신을 위한 추천 -->
+    <article class="main-products recommand">
+      <div class="title-wrap">
+        <h2 class="title">당신을 위한 추천</h2>
+      </div>
+      <!-- list이미지 -->
+      <div class="main-products-list">
+        <ul class="main-list">
+          <!-- list1 -->
+          <li class="thumb">
+            <a href="#">
+              <figure class="figure">
+                  <img src="#" alt="미쏘 글로우 플리츠">
+              </figure>
+              <div class="info">
+                  <p>미쏘</p>
+                  <strong>글로우 플리츠 스커트</strong>
+                  <span>17%</span>
+                  <em>25,560</em>
+              </div>
+            </a>
+          </li>
+          <!-- list2 -->
+          <li class="thumb">
+            <a href="#">
+              <figure class="figure">
+                  <img src="#" alt="알파카 부들 도톰니트">
+              </figure>
+              <div class="info">
+                  <p>글로우프로젝트</p>
+                  <strong>알파카 부들 도톰니트</strong>
+                  <span>11%</span>
+                  <em>44,000</em>
+              </div>
+            </a>
+          </li>
+          <!-- lsit3 -->
+          <li class="thumb">
+            <a href="#">
+              <figure class="figure">
+                  <img src="#" alt="글로우 플리츠 스커트">
+              </figure>
+              <div class="info">
+                  <p>프롬헤드투토</p>
+                  <strong>글로우 플리츠 스커트</strong>
+                  <span>11%</span>
+                  <em>32,650</em>
+              </div>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </article>
+
+    <!-- 놓칠 수 없는 브랜디 특가 -->
+    <article class="main-products special">
+      <div class="title-wrap">
+        <h2 class="title">놓칠 수 없는 브랜디 특가</h2>
+      </div>
+        <!-- list1 -->
+        <li class="thumb-a">
+          <a href="#">
+            <figure class="figure">
+                <img src="#" alt="덤블 뽀글이 리버시블 베이직...">
+            </figure>
+            <div class="bage-a">
+                <span class="bage">41%</span>
+            </div>
+            <div class="info">
+                <p>남은시간 123:05:55</p>
+                <strong>덤블 뽀글이 리버시블 베이직...</strong>
+                <em>75,880</em>
+            </div>
+          </a>
+        </li>
+        <!-- list2 -->
+        <li class="thumb-a">
+          <a href="#">
+            <figure class="figure">
+                <img src="#" alt="덤블 뽀글이 리버시블 베이직...">
+            </figure>
+            <div class="bage-a">
+                <span class="bage">41%</span>
+            </div>
+            <div class="info">
+                <p>남은시간 123:05:55</p>
+                <strong>덤블 뽀글이 리버시블 베이직...</strong>
+                <em>75,880</em>
+            </div>
+          </a>
+        </li>
+        <!-- list3 -->
+        <li class="thumb-a">
+          <a href="#">
+            <figure class="figure">
+                <img src="#" alt="덤블 뽀글이 리버시블 베이직...">
+            </figure>
+            <div class="bage-a">
+                <span class="bage">41%</span>
+            </div>
+            <div class="info">
+                <p>남은시간 123:05:55</p>
+                <strong>덤블 뽀글이 리버시블 베이직...</strong>
+                <em>75,880</em>
+            </div>
+          </a>
+        </li>
+    </article>
+
+    <!-- 하루 배송 상품은 내일도착 -->
+    <article class="main-products oneday">
+      <div class="title-wrap">
+        <figure>
+          <img src="#" alt="">
+        </figure>
+        <h2 class="title">상품은 내일 도착</h2>
+      </div>
+
+      <!-- list이미지 -->
+      <div class="main-products-list">
+        <ul class="main-list">
+          <!-- list1 -->
+          <li class="thumb">
+            <a href="#">
+              <figure class="figure">
+                  <img src="#" alt="미쏘 글로우 플리츠">
+              </figure>
+              <div class="bage">
+                  <figure>
+                    <img src="#" alt="하루배송">
+                  </figure>
+              </div>
+              <div class="info">
+                  <p>미쏘</p>
+                  <strong>글로우 플리츠 스커트</strong>
+                  <span>17%</span>
+                  <em>25,560</em>
+              </div>
+            </a>
+          </li>
+          <!-- list2 -->
+          <li class="thumb">
+            <a href="#">
+              <figure class="figure">
+                  <img src="#" alt="미쏘 글로우 플리츠">
+              </figure>
+              <div class="bage">
+                  <figure>
+                    <img src="#" alt="하루배송">
+                  </figure>
+              </div>
+              <div class="info">
+                  <p>미쏘</p>
+                  <strong>글로우 플리츠 스커트</strong>
+                  <span>17%</span>
+                  <em>25,560</em>
+              </div>
+            </a>
+          </li>
+          <!-- list3 -->
+          <li class="thumb">
+            <a href="#">
+              <figure class="figure">
+                  <img src="#" alt="미쏘 글로우 플리츠">
+              </figure>
+              <div class="bage">
+                  <figure>
+                    <img src="#" alt="하루배송">
+                  </figure>
+              </div>
+              <div class="info">
+                  <p>미쏘</p>
+                  <strong>글로우 플리츠 스커트</strong>
+                  <span>17%</span>
+                  <em>25,560</em>
+              </div>
+            </a>
+          </li>
+          <!-- list4 -->
+          <li class="thumb">
+            <a href="#">
+              <figure class="figure">
+                  <img src="#" alt="미쏘 글로우 플리츠">
+              </figure>
+              <div class="bage">
+                  <figure>
+                    <img src="#" alt="하루배송">
+                  </figure>
+              </div>
+              <div class="info">
+                  <p>미쏘</p>
+                  <strong>글로우 플리츠 스커트</strong>
+                  <span>17%</span>
+                  <em>25,560</em>
+              </div>
+            </a>
+          </li>
+          <!-- list5 -->
+          <li class="thumb">
+            <a href="#">
+              <figure class="figure">
+                  <img src="#" alt="미쏘 글로우 플리츠">
+              </figure>
+              <div class="bage">
+                  <figure>
+                    <img src="#" alt="하루배송">
+                  </figure>
+              </div>
+              <div class="info">
+                  <p>미쏘</p>
+                  <strong>글로우 플리츠 스커트</strong>
+                  <span>17%</span>
+                  <em>25,560</em>
+              </div>
+            </a>
+          </li>
+          <!-- list6 -->
+          <li class="thumb">
+            <a href="#">
+              <figure class="figure">
+                  <img src="#" alt="미쏘 글로우 플리츠">
+              </figure>
+              <div class="bage">
+                  <figure>
+                    <img src="#" alt="하루배송">
+                  </figure>
+              </div>
+              <div class="info">
+                  <p>미쏘</p>
+                  <strong>글로우 플리츠 스커트</strong>
+                  <span>17%</span>
+                  <em>25,560</em>
+              </div>
+            </a>
+          </li>
+          <!-- list7 -->
+          <li class="thumb">
+            <a href="#">
+              <figure class="figure">
+                  <img src="#" alt="미쏘 글로우 플리츠">
+              </figure>
+              <div class="bage">
+                  <figure>
+                    <img src="#" alt="하루배송">
+                  </figure>
+              </div>
+              <div class="info">
+                  <p>미쏘</p>
+                  <strong>글로우 플리츠 스커트</strong>
+                  <span>17%</span>
+                  <em>25,560</em>
+              </div>
+            </a>
+          </li>
+          <!-- list8 -->
+          <li class="thumb">
+            <a href="#">
+              <figure class="figure">
+                  <img src="#" alt="미쏘 글로우 플리츠">
+              </figure>
+              <div class="bage">
+                  <figure>
+                    <img src="#" alt="하루배송">
+                  </figure>
+              </div>
+              <div class="info">
+                  <p>미쏘</p>
+                  <strong>글로우 플리츠 스커트</strong>
+                  <span>17%</span>
+                  <em>25,560</em>
+              </div>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </article>
+
+    <div class="btn-common-main">
+      <a href="#"><span>하루배송 상품</span>더보기 ></a>
+    </div>
+
+    <!-- 지금 뜨고있는 브랜디 혜택 -->
+    <div class="main-products benefit">
+
+    </div>
+
+    <div class="btn-common-main">
+      <a href="#"><span>더 많은 기획전</span>보러가기 ></a>
+    </div>
   </div>
 </template>
 
