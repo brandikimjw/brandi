211e52146447d61986d8bd9142e1fcca9f3d9af4
kimjw5 <81278777+brandikimjw@users.noreply.github.com>
2021-04-08 18:13:28 +0900

    수정파일삭제

--- src/components/PageMain.vue
+++ src/components/PageMain.vue
@@ -1080,7 +1080,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-@import "~@/assets/scss/pages/main1";
+@import "~@/assets/scss/pages/main";
 .ir {
   height: 0;
   line-height: 0;
