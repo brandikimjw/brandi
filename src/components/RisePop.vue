53e5474b2d00b50171ff141caf9b33208a18f27c
kimjw5 <81278777+brandikimjw@users.noreply.github.com>
2021-04-13 13:22:50 +0900

    수정사항 반영

---
+++ src/components/RisePop.vue
@@ -0,0 +1,19 @@
+<template>
+<!-- 지금 뜨고있는 기획전 -->
+<section>
+  <div class="main-banner"></div>
+</section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+
+</style>
