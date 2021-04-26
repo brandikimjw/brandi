50bf8e10dfcc2e0882fea66b360dd75b3f0956c5
kimjw5 <81278777+brandikimjw@users.noreply.github.com>
2021-04-26 11:11:32 +0900

    newpage

---
+++ src/components/PageNewTab.vue
@@ -0,0 +1,25 @@
+<template></template>
+
+<script>
+// import MainSwiper from "./MainSwiper";
+
+export default {
+  name: "PageNew",
+  components: {
+    // MainSwiper,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+@import "~@/assets/scss/pages/main";
+
+.main-oneday {
+  .today-tit {
+  }
+}
+</style>
