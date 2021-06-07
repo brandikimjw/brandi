a74f2564c8701f7a29ddc73e0e3518a60d90d50d
김지원 <kimjw6@brandi.co.kr>
2021-06-07 15:29:40 +0900

    메인스와이퍼 수정

--- src/components/main/MainSwiper.vue
+++ src/components/main/MainSwiper.vue
@@ -1,284 +1,271 @@
 <template>
-  <div id="adaptive" class="main-banner">
-    <div ref="mainBanner" class="swiper-container">
+  <!-- Main-Swiper -->
+  <div class="main-banner-container">
+    <div class="swiper-container main-container">
       <ul class="swiper-wrapper">
-        <li class="swiper-slide" v-for="(item, index) in mainBannerList" :key="item.id">
-          <img :src="item.image_url" :key="index" alt="이미지슬라이드">
-          <!-- <img src="~@/assets/images/bannerImage03.jpeg" alt=""> -->
+        <li class="swiper-slide" v-for="(item, index) in mainBannerList" :key="index">
+          <img :src="item.image_url" :alt="`배너 슬라이드${index+1}`">
         </li>
       </ul>
-      <!-- 배너 버튼 영역 -->
+      <!-- 화살표 -->
       <button type="button" class="swiper-button-prev" aria-label="이전 슬라이드 이동"></button>
       <button type="button" class="swiper-button-next" aria-label="다음 슬라이드 이동"></button>
-      <!-- 매인 베너 paging 영역 -->
+      <!-- 플레이/스탑 -->
       <div class="banner-pager-box">
         <button type="button" @click="bannerAutoAction(true)" class="btn-banner-start skip" aria-label="배너 시작"></button>
         <button type="button" @click="bannerAutoAction(false)" class="btn-banner-stop" aria-label="배너 일시정지"></button>
-        <div class="swiper-pagination"></div>
+        <div class="swiper-pagination">
+        </div>
       </div>
     </div>
   </div>
 </template>
-<script>
 
+<script>
 export default {
-  name: "MainBanner",
   data() {
     return {
-      cnt : 0,
-      slideDisplay : 0,
+      mainSlide: "",
       mainBannerList : [
         {
           tit : '이미지 슬라이드1',
-          txt : '',
-          item : '',
-          // image_url : '../../assets/images/imagesbannerImage03.jpeg'
-          image_url : ('../../assets/images/imagesbannerImage03.jpeg')
-          // image_url : '/static/img/bannerImage03.5b7068b.jpeg'
+          image_url : 'https://placeimg.com/1000/454/any'
         },
         {
           tit : '이미지 슬라이드2',
-          txt : '',
-          item : '',
-          // image_url : '@/assets/images/bannerImage03.jpeg'
+          image_url : 'https://placeimg.com/1000/454/any'
+        },
+        {
+          tit : '이미지 슬라이드3',
+          image_url : 'https://placeimg.com/1000/454/any'
+        },
+        {
+          tit : '이미지 슬라이드4',
+          image_url : 'https://placeimg.com/1000/454/any'
+        },
+        {
+          tit : '이미지 슬라이드5',
+          image_url : 'https://placeimg.com/1000/454/any'
         },
       ]
     };
   },
-  methods : {
-    initSlider : function () {
-      // 슬라이더 실행
-      let that = this
-      let displayCount = 1
-      // 기존 배너 슬라이드 설정
-      let paginationOption = {
-          el : '#adaptive .swiper-pagination',
-          type : 'fraction',
-          renderFraction : function (currentClass, totalClass) {
-            return '<div class="short-type"><span class="' + currentClass + '"></span>' + ' / ' + '<span class="' + totalClass + '"></span></div>'
-          },
-      }
-      this.mainBanner = new Swiper('#adaptive .swiper-container', {
-        slidesPerView : displayCount,
-        autoHeight : true,
-        loop : true,
-        loopAdditionalSlides : 1,
-        a11y : false,
-        watchOverflow : true,
-        centeredSlides : true,
-        allowTouchMove : false,
-        speed : 600,
-        // autoplay : {
-        //   delay : 3000,
-        //   disableOnInteraction : false,
-        // },
-        navigation : {
-          prevEl : '#adaptive .swiper-button-prev',
-          nextEl : '#adaptive .swiper-button-next',
+  methods: {
+    initSlide() {
+      // swiper 관련 설정
+      this.mainSlide = new Swiper(".main-container", {
+        clickable: true,
+        slidesPerView: 1,
+        loop: true,
+        loopAdditionalSlides: 1,
+        loopFillGroupWithBlank: true,
+        centeredSlides: true,
+        autoHeight: true,
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: true,
         },
-        pagination : paginationOption,
-        breakpoints : {
-          768 : {
-            slidesPerView : 1,
-            allowTouchMove : true,
-            // 추후 모바일에서 불릿 타입이 아닌 텍스트 타입의 페이징을 사용시 아래의 내용을 주석해제하면 됨
-            pagination : {
-              type : 'fraction',
-              renderFraction : function (currentClass, totalClass) {
-                return '<div class="short-type"><span class="' + currentClass + '"></span>' + ' / ' + '<span class="' + totalClass + '"></span></div>'
-              },
-            },
-          },
+        pagination: {
+          el: ".swiper-pagination",
+          type: "fraction",
+          clickable: true,
         },
-        on : {
-          init : function () {
-            that.setBanner()
-            that.slideDisplay = displayCount
-          },
+        navigation: {
+          // 버튼 사용자 지정
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
         },
-      })
+      });
     },
-    setBanner : function () {
-      this.bindClickEvent()
+    pauseBanner() {
+      console.log("this.mainSlide:", this.mainSlide);
     },
-    bindClickEvent : function () {
-      let that = this
-      let mainBannerList = this.mainBannerList
-    },
-    bannerAutoAction : function (chk) {
-      let bannerStart = document.querySelector('.banner-pager-box .btn-banner-start')
-      let bannerStop = document.querySelector('.banner-pager-box .btn-banner-stop')
+    bannerAutoAction(chk) {
+      console.log(chk);
+      let bannerStart = document.querySelector('.btn-banner-start')
+      let bannerStop = document.querySelector('.btn-banner-stop')
       if (chk) {
-        this.mainBanner.autoplay.start()
+        this.mainSlide.autoplay.start()
         bannerStart.classList.add('skip')
         bannerStop.classList.remove('skip')
       } else {
-        this.mainBanner.autoplay.stop()
+        this.mainSlide.autoplay.stop()
         bannerStart.classList.remove('skip')
         bannerStop.classList.add('skip')
       }
     },
   },
   mounted() {
-    const vm = this
-    this.initSlider()
+    // swiper 실행
+    this.initSlide();
   },
-  computed : {
-    // mainBannerList() {
-
-    // }
-  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-#adaptive {
-  position: relative;
-  z-index: 2;
-  background-color:#f5f5f5;
-  overflow: hidden;
-  opacity: 0;
-  &.loaded {
-    opacity: 1;
-  }
-}
-.swiper-wrapper {
-  transition: all 0.7s cubic-bezier(0.645, 0.045, 0.355, 1.000);
+.main-banner-container {
   .swiper-slide {
-    text-align: center;
+    display: inline-block;
+    display: block;
+    line-height: 0;
     img {
       width: 100%;
-      height: auto;
-      margin-top: 0;
-      vertical-align: top;
+      height: inherit;
       cursor: pointer;
     }
   }
 }
-.swiper-pagination {
-    @include clearfix();
-    bottom: 15px;
-    left: 0;
-    width: 100%;
-    padding: 0 15px;
-    text-align: right;
-    .swiper-pagination-bullet {
-      overflow: hidden;
-      display: inline-block;
-      width: 8px;
-      height: 8px;
-      margin-left: 8px;
-      font-size: 0;
-      text-indent: -9999em;
-      border-radius: 50%;
-      background: #222;
-      opacity: 1;
-      transition: all 0.1s linear 0s;
-      &:first-child {
-        margin-left: 0;
-      }
-      &.swiper-pagination-bullet-active {
-        background: #ff204b;
-      }
-    }
-    .short-type{
-      float:right;
-      min-width: 58px;
-      padding:3px 4px 3px;
-      border-radius:12px;
-      background-color:rgba(32, 36, 41, 0.8);
-      color:#fff;
-      font-size:12px;
-      text-align:center;
-    }
-  }
-  .swiper-button-prev,
-  .swiper-button-next {
+.swiper-button-prev {display: none}
+.swiper-button-next {display: none}
+.banner-pager-box{
+  @include clearfix();
+  position: absolute;
+  bottom: 12px;
+  right: 16px;
+  z-index: 100;
+  button{
+    float: left;
     appearance: none;
-    display: none;
-    position: absolute;
-    top: 50%;
-    width: 32px;
-    height: 32px;
-    margin-top: 0;
-    border: none;
-    border-radius: 0;
+    display: block;
+    width: 24px;
+    height: 24px;
+    margin: 0 4px 0 0;
+    padding: 0;
+    border: 0;
+    border-radius: 50%;
     text-shadow: none;
-    background: url('#{$imgUrl}/controls.png') no-repeat;
-    transform: translateY(-50%);
+    background: rgba(32, 36, 41, 0.8);
     cursor: pointer;
-    opacity: 0.5;
-    z-index: 99;
-    &:hover {
-      opacity: 1;
+    &.skip{
+      overflow: hidden;
+      position: absolute;
+      left: -9999px;
+      width: 1px;
+      height: 1px;
+      font-size: 0;
+      line-height: 0;
+    }
+    &.btn-banner-start{
+      background: url(~@/assets/images/ic-stop-r@3x.png) no-repeat center rgba(32, 36, 41, 0.8);
+      background-size: 50%;
+    }
+    &.btn-banner-stop{
+      background: url(~@/assets/images/ic-play-r@3x.png) no-repeat center rgba(32, 36, 41, 0.8);
+      background-size: 50%;
     }
   }
-  .swiper-button {
-    &-prev {
-      left: 10px;
-      background-position: 0 0;
-    }
-    &-next {
-      right: 10px;
-      background-position: -32px 0;
-    }
+  .swiper-pagination{
+    float: left;
+    position: static;
+    width: auto;
+    padding: 0;
+    transform: translateX(0);
+    background-color: rgba(32, 36, 41, 0.8);
+    padding: 6px 10px 7px;
+    font-size: 11px;
+    color: #fff;
+    border-radius:12px;
   }
-  &.main-banner{
-    margin-bottom: 50px;
-    .swiper-container{
-      .banner-pager-box{
-        @include clearfix();
+}
+@media screen and (min-width: map-get($breakpoints, "medium")) {
+  .main-banner-container {
+    .swiper-container {
+      overflow: visible;
+      width: 83%;
+      margin: 0 auto;
+      &::before {
+        content: "";
+        display: block;
+        width: 1000%;
         position: absolute;
-        bottom: 12px;
-        right: 16px;
-        z-index: 100;
-        button{
-          float: left;
-          appearance: none;
-          display: block;
-          width: 24px;
-          height: 24px;
-          margin: 0 4px 0 0;
-          padding: 0;
-          border: 0;
-          border-radius: 50%;
-          text-shadow: none;
-          background: rgba(32, 36, 41, 0.8);
-          cursor: pointer;
-          &.skip{
-            overflow: hidden;
-            position: absolute;
-            left: -9999px;
-            width: 1px;
-            height: 1px;
-            font-size: 0;
-            line-height: 0;
-          }
-          &.btn-banner-start{
-            background: url(~@/assets/images/ic-play-r@3x.png) no-repeat;
-            background-size: 50%;
-          }
-          &.btn-banner-stop{
-            background: url(~@/assets/images/ic-stop-r@3x.png) no-repeat;
-            //background: url('#{$imgUrl}/ic-stop-m@3x.png') no-repeat center rgba(32, 36, 41, 0.8);
-            background-size: 50%;
-          }
-        }
-        .swiper-pagination{
-          float: left;
-          position: static;
-          width: auto;
-          padding: 0;
-          transform: translateX(0);
-          .short-type{
-            min-width: auto;
-            float: none;
-            padding: 6px 10px 7px;
-            font-size: 11px;
-            line-height: 1;
-          }
-        }
+        top: 0;
+        bottom: 0;
+        background: rgba(0, 0, 0, 0.5);
+        z-index: 10;
+        left: -1000%;
+      }
+      &::after {
+        content: "";
+        display: block;
+        width: 1000%;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        background: rgba(0, 0, 0, 0.5);
+        z-index: 1;
+        right: -1000%;
       }
     }
   }
+  .banner-pager-box {
+    button {
+      width: 38px;
+      height: 38px;
+    }
+    .swiper-pagination {
+      padding: 11.5px 21.5px 12.5px;
+      border-radius: 30px;
+      font-size: 14px;
+      margin: 0 8px 8px 0;
+    }
+  }
+  .swiper-button-next:after, .swiper-container-rtl .swiper-button-prev:after {
+    display: none;
+  }
+  .swiper-button-prev:after, .swiper-container-rtl .swiper-button-next:after {
+    display: none;
+  }
+  .swiper-container {
+    .swiper-button-prev {
+      display: block;
+      appearance: none;
+      position: absolute;
+      top: 50%;
+      left: -30px;
+      width: 62px;
+      height: 62px;
+      // border: none;
+      border-radius: 0;
+      text-shadow: none;
+      background: url(~@/assets/images/ic-arrow-l@3x.png) no-repeat center/140%;
+      transform: translateY(-50%);
+      cursor: pointer;
+      z-index: 900;
+    }
+    .swiper-button-next {
+      display: block;
+      appearance: none;
+      position: absolute;
+      top: 50%;
+      right: -30px;
+      width: 62px;
+      height: 62px;
+      margin-top: 0;
+      border: none;
+      border-radius: 0;
+      text-shadow: none;
+      background: url(~@/assets/images/ic-arrow-r@3x.png) no-repeat center/140%;
+      transform: translateY(-50%);
+      cursor: pointer;
+      z-index: 900;
+    }
+  }
+}
+@media screen and (min-width: map-get($breakpoints, "large")) {
+  .main-banner-container {
+    .main-container {
+      width: 1000px;
+    }
+  }
+  .main-banner-box {
+    .pagination-box {
+      .swiper-pagination {
+        padding: 11px 21px 12px;
+        border-radius: 30px;
+        font-size: 14px;
+      }
+    }
+  }
+}
+</style>
